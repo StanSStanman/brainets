18fe82537dc2713c735a07f9e5eb43559bc2219e
EtienneCmb <e.combrisson@gmail.com>
2019-02-14 16:07:15 +0100

    HGA using Julien's parameters

--- brainets/spectral.py
+++ brainets/spectral.py
@@ -54,3 +54,34 @@ def mt_hga(epoch, f=100., n_cycles=12, time_bandwidth=20., **kw):
     tf = tfr_multitaper(epoch, freq, freq / n_cycles, return_itc=False,
                         time_bandwidth=time_bandwidth, **kw)
     return tf
+
+
+def mt_hga_julien(epoch, f=100., n_cycles=12, time_bandwidth=4., **kw):
+    """Extract high-gamma activity (HGA) around a central frequency.
+
+    The HGA is extracted using the `mne.time_frequency.tfr_multitaper`
+    function but the parameters provided by Julien Bastin.
+
+    Parameters
+    ----------
+    epoch : mne.Epochs
+        Instance of mne.Epochs
+    f : float | 100.
+        The central high-gamma frequency
+    n_cycles : int | 12
+        The number of cycles to use for the frequency resolution.
+    time_bandwidth : float | 20.
+        Time x (Full) Bandwidth product.
+    karg : dict | {}
+        Additional arguments are passed to the `tfr_multitaper` function.
+
+    Returns
+    -------
+    tf : AverageTFR | EpochsTFR
+        The averaged or single-trial HGA.
+    """
+    assert isinstance(f, (int, float))
+    freq = np.array([f])
+    tf = tfr_multitaper(epoch, freq, .2 * freq, return_itc=False,
+                        time_bandwidth=time_bandwidth, **kw)
+    return tf
