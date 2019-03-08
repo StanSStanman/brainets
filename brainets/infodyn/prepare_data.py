5f388adf928b57106182961fd3387a6cf4af3275
EtienneCmb <e.combrisson@gmail.com>
2019-03-08 02:02:05 +0100

    Reorganize the data by roi

--- brainets/infodyn/prepare_data.py
+++ brainets/infodyn/prepare_data.py
@@ -2,7 +2,7 @@
 import logging
 
 import numpy as np
-from xarray import DataArray
+from xarray import DataArray, concat
 import mne
 
 from brainets.gcmi import copnorm
@@ -10,9 +10,16 @@ from brainets.gcmi import copnorm
 logger = logging.getLogger('brainets')
 
 
-def gcmi_prepare_data(data, dp, roi, times=None, gcrn=True, aggregate='mean',
-                      modality='meg', verbose=None):
+def gccmi_prepare_data(data, dp, roi, times=None, gcrn=True, aggregate='mean',
+                       modality='meg', verbose=None):
     """Prepare the M/SEEG data before computing the GCMI.
+
+    This function performs the following steps :
+
+        * convert the list of inputs to standard NumPy arrays
+        * mean channels that belong to the same roi per subject
+        * apply a gaussian copula rank normalization
+        * re-organize the output by roi
 
     Parameters
     ----------
@@ -32,7 +39,7 @@ def gcmi_prepare_data(data, dp, roi, times=None, gcrn=True, aggregate='mean',
         MNE instances are provided, the time vector is inferred from it
     gcrn : bool | True
         Apply a Gaussian copula rank normalization.
-    aggregate : {'mean', 'concat', None}
+    aggregate : {'mean', 'concat'}
         Strategy to group sensors / channels inside an ROI. Choose either
         'mean' (e.g. mean HGA inside the roi) or 'concat' to concatenate all
         sites inside this roi
@@ -40,7 +47,7 @@ def gcmi_prepare_data(data, dp, roi, times=None, gcrn=True, aggregate='mean',
     Returns
     -------
     data : list
-        List of DataArray
+        List of DataArray organize by roi
     """
     assert all([isinstance(k, (list, tuple)) for k in (data, dp)])
     assert len(data) == len(dp)
@@ -55,12 +62,33 @@ def gcmi_prepare_data(data, dp, roi, times=None, gcrn=True, aggregate='mean',
         data = [k.groupby('roi').mean('roi') for k in data]
     elif aggregate == 'concat':
         raise NotImplementedError()
+    else:
+        raise ValueError("aggregate shoud either be 'mean' or 'concat'")
     # gaussian copula rank normalization
     if gcrn:
         logger.info("    Apply the Gaussian Copula rank normalization")
         for d in data:
             d.data = copnorm(d.data)
-    return data
+    # reorganize the data by roi instead of subject
+    logger.info("    Re-organize by roi")
+    rois = [d.roi.values.tolist() for d in data]
+    u_rois = np.unique(np.r_[tuple(rois)])
+    x = []
+    for r in u_rois:
+        _x, _s, _s_id, q = [], [], [], 0
+        for d in data:
+            if r in d.roi.values:  # find if the ROI is present (sEEG)
+                _x += [d.loc[:, r, :]]
+                _s += [d.name]
+                _s_id += [q] * d.shape[0]
+                q += 1
+        # Concatenate DataArray and attach additional attributes
+        _c = concat(_x, dim='dp')
+        _c.name = r
+        _c.attrs['subjects'] = _s
+        _c.attrs['z'] = np.array(_s_id)
+        x += [_c]
+    return x
 
 
 def _prepare_single_subject(x, dp, roi, times, n):
@@ -103,8 +131,9 @@ if __name__ == '__main__':
 
     def generate_data(n_trials, n_channels, n_pts, n_roi=3):  # noqa
         x = np.random.rand(n_trials, n_channels, n_pts)
-        dp = np.random.rand(n_trials)
-        roi = np.random.randint(0, n_roi, n_channels)
+        dp = np.arange(n_trials)
+        # dp = np.random.rand(n_trials)
+        roi = ['roi%i' % k for k in np.random.randint(0, n_roi, n_channels)]
         times = np.linspace(-1.4, 1.4, n_pts, endpoint=True)
         return x, dp, roi, times
 
@@ -112,11 +141,11 @@ if __name__ == '__main__':
     # Dataset 1
     x_1, dp_1, roi_1, times = generate_data(50, 20, 100)
     # Dataset 2
-    x_2, dp_2, roi_2, times = generate_data(47, 37, 100)
+    x_2, dp_2, roi_2, times = generate_data(47, 37, 100, 5)
     # Concatenate datasets
     x = [x_1, x_2]
     dp = [dp_1, dp_2]
     roi = [roi_1, roi_2]
-    data = gcmi_prepare_data(x, dp, roi, times=times, aggregate=None)
-    print(data[0].shape)
+    data = gccmi_prepare_data(x, dp, roi, times=times, aggregate='mean')
+    print(data[0].T)
     # -------------------------------------------------------------------------
